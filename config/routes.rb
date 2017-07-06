b9aa27f660372484ac090bf7ff6a678e07bfcc9d
Your Name <you@example.com>
2017-07-06 17:23:47 +0800

    Implement easy chat

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   resources :friendships
   resources :users, only: [:index]
 
+  resources :conversations do
+    resources :messages
+  end
   root "friendships#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
